7a9a44681de04dbebcc80eeeb13669b5b24be7ba
Luis Miguel Serrano <luis.miguel.santos.serrano@gmail.com>
2023-01-26 16:42:59 +0000

    Add "search-restaurants.tests.js" and update "when.js" tooling to generate events and context with requestId at the expected paths used by the EMF metrics in use for search-restaurants

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,10 +1,11 @@
 const APP_ROOT = '../../'
 const _ = require('lodash')
+const { v4: uuidv4 } = require('uuid');
 
 const viaHandler = async (event, functionName) => {
     const handler = require(`${APP_ROOT}/functions/${functionName}`).handler
 
-    const context = {}
+    const context = { requestId: event.requestContext.requestId }
     const response = await handler(event, context)
     const contentType = _.get(response, 'headers.Content-Type', 'application/json');
     if (response.body && contentType === 'application/json') {
@@ -13,10 +14,22 @@ const viaHandler = async (event, functionName) => {
     return response
 }
 
-const we_invoke_get_index = () => viaHandler({}, 'get-index')
-const we_invoke_get_restaurants = () => viaHandler({}, 'get-restaurants')
+const generateEvent = eventContents => {
+    const event = { requestContext: { requestId: uuidv4({},{}, {}) }}
+    return {...event, ...eventContents};
+}
+
+const we_invoke_get_index = () => viaHandler(generateEvent(), 'get-index')
+const we_invoke_get_restaurants = () => viaHandler(generateEvent(), 'get-restaurants')
+const we_invoke_search_restaurants = theme => {
+    let event = generateEvent({
+        body: JSON.stringify({ theme })
+    })
+    return viaHandler(event, 'search-restaurants')
+}
 
 module.exports = {
     we_invoke_get_index,
-    we_invoke_get_restaurants
+    we_invoke_get_restaurants,
+    we_invoke_search_restaurants
 }
