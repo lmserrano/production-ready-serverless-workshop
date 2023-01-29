4a986f8d2811588674c5c40ef67932780f686e0e
Luis Miguel Serrano <luis.miguel.santos.serrano@gmail.com>
2023-01-29 17:01:20 +0000

    Update Jest configuration to allow easily changing test timeouts, which is useful for debugging purposes

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test_cases/**/*'],
-  setupFiles: ["./tests/setup/setup-tests.js"]
+  setupFiles: ["./tests/setup/setup-tests.js"],
+  testTimeout: 5000 // Default: 5000 milliseconds
 }
